b3ff8e23f5587e724853b1a8fd6e6266fa3c97ef
Eric Stern <eric@ericstern.com>
2016-07-24 21:52:26 -0700

    Add SeededRandomizer

---
+++ LTTPRandomizer/SeededRandomizer.swift
@@ -0,0 +1,19 @@
+//
+//  SeededRandomizer.swift
+//  LTTPRandomizer
+//
+//  Created by Eric Stern on 7/24/16.
+//  Copyright © 2016 Eric Stern. All rights reserved.
+//
+
+import Foundation
+
+struct SeededRandomizer {
+    let seed: Int
+
+
+    func next(max: Int) -> Int {
+        return Int(arc4random_uniform(UInt32(max)))
+//        return 0
+    }
+}
