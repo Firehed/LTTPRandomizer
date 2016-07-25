c17b10d89ba13bdfebdab4e39ca3e83a689c0de1
Eric Stern <eric@ericstern.com>
2016-07-24 23:56:01 -0700

    Do something on launch

--- LTTPRandomizer/ViewController.swift
+++ LTTPRandomizer/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let r = RomBuilder(seed: 0, locations: DifficultyCasual())
+        r.assignItems()
+        r.write()
     }
 
     override var representedObject: AnyObject? {
