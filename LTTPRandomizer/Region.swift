2666bfa666736630d3e2930b1decebbaf0ae97a5
Eric Stern <eric@ericstern.com>
2016-07-24 21:51:13 -0700

    Add Region enum

---
+++ LTTPRandomizer/Region.swift
@@ -0,0 +1,28 @@
+//
+//  Location.swift
+//  LTTPRandomizer
+//
+//  Created by Eric Stern on 7/23/16.
+//  Copyright © 2016 Eric Stern. All rights reserved.
+//
+
+import Foundation
+
+enum Region: Int {
+    case Progression
+    case HyruleCastleEscape
+    case LightWorld
+    case EasternPalace
+    case DesertPalace
+    case TowerOfHera
+    case HyruleCastleTower
+    case DarkWorld
+    case DarkPalace
+    case SwampPalace
+    case SkullWoods
+    case ThievesTown
+    case IcePalace
+    case MiseryMire
+    case TurtleRock
+    case GanonsTower
+}
