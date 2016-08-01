978e49b733dd6a78e15c9fea89812bb547c93222
Eric Stern <eric@ericstern.com>
2016-07-31 19:49:59 -0700

    Add documentation to Difficulty protocol

--- LTTPRandomizer/Difficulty.swift
+++ LTTPRandomizer/Difficulty.swift
@@ -12,7 +12,24 @@ protocol Difficulty {
     var locations: [Location] { get }
     func reset() -> Void
     func getItemPool(random: Randomizer) -> [Item]
-    func getUnavailableLocations(have: Set<Item>) -> [Location]
+
+    /**
+        Given a pool of items and currently-accessible locations, returns an item to place.
+        - parameters:
+          - possibleItems: the item pool
+          - possibleLocations: the currently accessible locations
+          - randomizer: a randomizer
+        - returns: the Item to place
+    */
     func getItemForInsertion(possibleItems: [Item], possibleLocations: [Location], randomizer: Randomizer) -> Item
+
+    /**
+        Given an item to place and currently-accessible locations, return the location to place the item
+        - parameters:
+            - possibleLocations: the currently accessible locations
+            - item: the Item being placed
+            - randomizer: a randomizer
+        - returns: the Location to place the Item
+    */
     func getLocationForItemPlacement(possibleLocations: [Location], item: Item, randomizer: Randomizer) -> Location
 }
